d8afcda08109a3d95ddb5faf440829e6c7e853f8
SinaSystem <sinasystem47@gmail.com>
2020-06-04 22:45:48 +0430

    Initial commit

---
+++ app/src/main/java/com/sriyanksiddhartha/servicesdemo/MainActivity.java
@@ -0,0 +1,107 @@
+package com.sriyanksiddhartha.servicesdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * 	Author: Sriyank Siddhartha
+ *
+ * 	Module 6: Working with Bound Service
+ *
+ *		"AFTER" project
+ * */
+public class MainActivity extends AppCompatActivity {
+
+	private TextView txvIntentServiceResult, txvStartedServiceResult;
+
+	private Handler handler = new Handler();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		txvIntentServiceResult = (TextView) findViewById(R.id.txvIntentServiceResult);
+		txvStartedServiceResult = (TextView) findViewById(R.id.txvStartedServiceResult);
+	}
+
+	public void startStartedService(View view) {
+
+		Intent intent = new Intent(MainActivity.this, MyStartedService.class);
+		intent.putExtra("sleepTime", 10);
+		startService(intent);
+	}
+
+	public void stopStartedService(View view) {
+
+		Intent intent = new Intent(MainActivity.this, MyStartedService.class);
+		stopService(intent);
+	}
+
+
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction("action.service.to.activity");
+		registerReceiver(myStartedServiceReceiver, intentFilter);
+	}
+
+	// To receive the data back from MyStartedService.java using BroadcastReceiver
+	private BroadcastReceiver myStartedServiceReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+
+			String result = intent.getStringExtra("startServiceResult");
+			txvStartedServiceResult.setText(result);
+		}
+	};
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		unregisterReceiver(myStartedServiceReceiver);
+	}
+
+
+
+	// To receive the data back from MyIntentService.java using ResultReceiver
+	private class MyResultReceiver extends ResultReceiver {
+
+		public MyResultReceiver(Handler handler) {
+			super(handler);
+		}
+
+		@Override
+		protected void onReceiveResult(int resultCode, Bundle resultData) {
+			super.onReceiveResult(resultCode, resultData);
+
+			Log.i("MyResultreceiver", Thread.currentThread().getName());
+
+			if (resultCode == 18 && resultData != null) {
+
+				final String result = resultData.getString("resultIntentService");
+
+				handler.post(new Runnable() {
+					@Override
+					public void run() {
+						Log.i("MyHandler", Thread.currentThread().getName());
+						txvIntentServiceResult.setText(result);
+					}
+				});
+			}
+		}
+	}
+}
